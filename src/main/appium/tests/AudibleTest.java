5ffca4482e8d1c18596c5f8f831b7875f7bd66fd
pcreddeppa <pcreddepa@atimi.com>
2023-01-25 13:25:14 +0530

    Created a test class and base page with separate packages. Created a test to launch the application.

---
+++ src/main/appium/tests/AudibleTest.java
@@ -0,0 +1,17 @@
+package src.main.appium.tests;
+
+import org.testng.annotations.Test;
+
+/**
+ * This is 'AudibleTest' class.
+ */
+public class AudibleTest extends SampleTest {
+
+    /**
+     * Test to launch application.
+     */
+    @Test
+    public void testLaunchApplication() {
+        System.out.println("Application is started..");
+    }
+}
