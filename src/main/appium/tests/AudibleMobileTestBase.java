c6fbf15664ae893916b7b7bc8e4e7a517869140a
pcreddeppa <pcreddepa@atimi.com>
2023-01-30 12:14:32 +0530

    Changes in driver set up for before class and method. Created common method for  android and IOS. Added javaDocs.

--- src/main/appium/tests/AudibleMobileTestBase.java
+++ src/main/appium/tests/AudibleMobileTestBase.java
@@ -1,13 +1,55 @@
 package src.main.appium.tests;
 
-import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.AppiumDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 /**
  * This is 'AudibleMobileTestBase' abstract class.
  */
 public abstract class AudibleMobileTestBase {
 
-    public abstract AndroidDriver createDriver();
+    private AppiumDriver driver = null;
 
-    public abstract AndroidDriver getDriver();
+    protected abstract AppiumDriver createDriver();
+
+    /**
+     * Gets the driver.
+     *
+     * @return driver
+     */
+    public AppiumDriver getDriver() {
+        return driver;
+    }
+
+    /**
+     * Set up the session before class.
+     */
+    @BeforeClass
+    public final void setUpSessionBeforeClass() {
+        setUpDriver();
+    }
+
+    /**
+     * Set up the session before method.
+     */
+    @BeforeMethod
+    public final void setUpSessionBeforeMethod() {
+        setUpDriver();
+    }
+
+    /**
+     * Set up the driver.
+     */
+    private void setUpDriver() {
+        try {
+            if (driver != null) {
+                driver.quit();
+            } else {
+                driver = createDriver();
+            }
+        } catch (final Exception ex) {
+            throw ex;
+        }
+    }
 }
