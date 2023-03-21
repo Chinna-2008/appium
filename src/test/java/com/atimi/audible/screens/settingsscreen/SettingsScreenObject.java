73200e2f795391554a9a66737a4a0ae01a382c89
dbshivanagoudra <dbshivanagoudra@atimi.com>
2023-03-21 15:19:20 +0530

    Created 'AppSettingsOptions' enum. Wrote test script to validate the data and storage option. Used stream limit method. Added java docs.

--- src/test/java/com/atimi/audible/screens/settingsscreen/SettingsScreenObject.java
+++ src/test/java/com/atimi/audible/screens/settingsscreen/SettingsScreenObject.java
@@ -6,6 +6,7 @@ import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import lombok.Data;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import java.util.List;
 
 /**
  * This is 'SettingsScreenObject' class.
@@ -17,6 +18,12 @@ public class SettingsScreenObject {
 
     @FindBy(id = "com.audible.application:id/top_bar_title")
     private MobileElement settingsScreen;
+
+    @FindBy(id = "com.audible.application:id/row_label")
+    private List<MobileElement> appSettingsOptions;
+
+    @FindBy(id = "com.audible.application:id/top_bar_title")
+    private MobileElement storageAndStorageScreen;
 
     public SettingsScreenObject(final AppiumDriver driver) {
         this.driver = driver;
--- src/test/java/com/atimi/tests/android/ProfileSettingsScreenTest.java
+++ src/test/java/com/atimi/tests/android/ProfileSettingsScreenTest.java
@@ -23,4 +23,21 @@ public class ProfileSettingsScreenTest extends AndroidTestBase {
         getProfileScreen().tapSettings();
         Assert.assertTrue(getSettingsScreen().isSettingsScreenDisplayed(), "Unable to load the settings screen.");
     }
+
+    /**
+     * Tests data and storage option.
+     */
+    @Test
+    public void testDataAndStorageOption() {
+        getWelcomeScreen().waitForScreenToLoad();
+        getWelcomeScreen().tapSignIn();
+        getSignInScreen().waitForScreenToLoad();
+        getSignInScreen().signIn("reddeppapc1@gmail.com", "Reddeppa@3");
+        getHomeScreen().waitForScreenToLoad();
+        getHomeScreen().tapMenuTab(MenuTabs.PROFILE.getTabsValue());
+        getProfileScreen().tapSettings();
+        getSettingsScreen().waitForScreenToLoad();
+        getSettingsScreen().tapAppSettingsOption(AppSettingsOptions.DATA_AND_STORAGE.getAppSettingsOptionsValue());
+        Assert.assertTrue(getSettingsScreen().isDataAndStorageScreenDisplayed(), "Unable to load the data and storage screen.");
+    }
 }
