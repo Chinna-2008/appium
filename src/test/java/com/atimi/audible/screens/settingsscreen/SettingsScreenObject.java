8c7a496ef788bea7ebc669110b8652be06750c49
dbshivanagoudra <dbshivanagoudra@atimi.com>
2023-04-05 17:13:48 +0530

    Updated the test scripts based on review comments. Removed 'AppSettingsOptions' enum.

--- src/test/java/com/atimi/audible/screens/settingsscreen/SettingsScreenObject.java
+++ src/test/java/com/atimi/audible/screens/settingsscreen/SettingsScreenObject.java
@@ -2,11 +2,11 @@ package com.atimi.audible.screens.settingsscreen;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import lombok.Data;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import java.util.List;
 
 /**
  * This is 'SettingsScreenObject' class.
@@ -19,11 +19,8 @@ public class SettingsScreenObject {
     @FindBy(id = "com.audible.application:id/top_bar_title")
     private MobileElement settingsScreen;
 
-    @FindBy(id = "com.audible.application:id/row_label")
-    private List<MobileElement> appSettingsOptions;
-
-    @FindBy(id = "com.audible.application:id/top_bar_title")
-    private MobileElement storageAndStorageScreen;
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Data & Storage\")")
+    private MobileElement dataAndStorageOption;
 
     public SettingsScreenObject(final AppiumDriver driver) {
         this.driver = driver;
